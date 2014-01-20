d5bf2aa79c0dd077f9a679436bd7ebbf92128bd6
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2014-01-20 13:55:38 +0100

    first release of module

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,11 +35,46 @@
 #
 # Copyright 2014 Your name here, unless otherwise noted.
 #
-class pcns {
+class pcns (
+  $NMCIP,
+  $NMCusername,
+  $NMCpassword,
+  $NMSauthenticationPhrase,
+  ){
 
-  class { 'java':
-    distribution => 'jre',
-    version      => '7u45',
+ file { "/tmp/install.sh":
+    content       => template('pcns/install.sh.erb'),
+    mode          => '0700',
   }
 
+ file { "/tmp/silentInstall":
+    content       => template('pcns/silentInstall.erb'),
+    mode          => '0640',
+  }
+
+  file {"/tmp/jre-7u45-linux-x64.tar.gz":
+    source        => "puppet:///modules/pcns/jre-7u45-linux-x64.tar.gz",
+    ensure        => "present",
+  }
+
+  file {"/tmp/pcns310.tar.gz":
+    source        => "puppet:///modules/pcns/pcns310.tar.gz",
+    ensure        => "present",
+  }
+
+  exec {"extract-java":
+    command   => "/bin/tar -xzf /tmp/jre-7u45-linux-x64.tar.gz",
+    cwd       => "/opt",
+    unless    => "/usr/bin/test -d /opt/jre1.7.0_45",
+    require   => File["/tmp/jre-7u45-linux-x64.tar.gz"],  
+  }
+
+  exec {"run-install-script":
+    command   => "/tmp/install.sh -f /tmp/silentInstall",
+    cwd       => "/tmp",
+    unless    => "/usr/bin/test -d /opt/APC",
+    require   => [Exec["extract-java"],
+                  File["/tmp/install.sh"],
+                  File["/tmp/pcns310.tar.gz"]],
+  }
 }
